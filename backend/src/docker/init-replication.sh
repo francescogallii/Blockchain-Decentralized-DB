4f56295660e5cb716e23bf38e62ed73800b967bf
francescogallii <francescogalli0407@gmail.com>
2025-10-25 19:00:14 +0200

    Rimossi file inutili

--- backend/src/docker/init-replication.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  CREATE ROLE $REPL_USER WITH REPLICATION LOGIN PASSWORD '$REPL_PASSWORD';
-EOSQL
-
-echo "host replication $REPL_USER all md5" >> "$PGDATA/pg_hba.conf"
